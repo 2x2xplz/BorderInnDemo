7c17db92cd002dd4f7e28c71f6a94b7cd85f226a
Andy Goldberg <andygoldberg@gmail.com>
2021-08-18 19:22:39 -0700

    hoplite non-classpath file loading required a little more work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,34 @@
+import com.sksamuel.hoplite.ConfigFilePropertySource
 import com.sksamuel.hoplite.ConfigLoader
+import com.sksamuel.hoplite.ConfigSource
 import com.sksamuel.hoplite.PropertySource
 import com.sksamuel.hoplite.json.JsonPropertySource
 import com.zaxxer.hikari.HikariDataSource
 import org.http4k.serverless.ApiGatewayV2LambdaFunction
 import org.ktorm.database.Database
 import org.ktorm.support.postgresql.PostgreSqlDialect
+import java.io.File
 
 
 // Hoplite setup
 data class GeoAPI(val apiKey: String, val host: String = "https://api.openrouteservice.org")
-data class DBConfig(val url: String, val username: String, val password: String)
+data class DBConfig(val url: String = "jdbc:h2:mem:", val username: String, val password: String)
 data class Destination(val name: String, val lat: Float, val lon: Float, val timeZone: String)
 data class AppConfig(val portNumber: Int = System.getenv("PORT")?.toInt() ?: 0,
                      val platform: String = "dev",
                      val destination: Destination, val db: DBConfig, val geo: GeoAPI)
 val config: AppConfig = ConfigLoader.Builder()
-    .addSource(JsonPropertySource(System.getenv("HOPLITE_JSON") ?: "{}"))
-    .addSource(PropertySource.resource(System.getenv("HOPLITE_FILENAME") ?: "/config_dev.yaml", optional = true))
+    .addSource(JsonPropertySource(System.getenv("HOPLITE_JSON") ?: System.getProperty("HOPLITE_JSON") ?: "{}"))
+    .apply {
+        System.getenv("HOPLITE_FILENAME") ?: System.getProperty("HOPLITE_FILENAME")?.let { configFile ->
+            if (!File(configFile).exists())
+                throw Exception("specified config file ($configFile) doesn't exist")
+            this.addPropertySource(
+                ConfigFilePropertySource(
+                    ConfigSource.FileSource(File(configFile)), optional = true))
+        }
+    }
+    .addSource(PropertySource.resource("/config_dev.yaml", optional = true))
     .build()
     .loadConfigOrThrow()
 
@@ -45,5 +57,5 @@ class GatewayListener : ApiGatewayV2LambdaFunction(appRoutes)
 fun main() {
     println("starting app...")
     serverApp.start()
-    println("app has started")
+    println("app has started on port ${config.portNumber} at ${config.destination.name}")
 }
